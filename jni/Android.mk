ffc89a5f4ef2925fe2a65cccc12bc93b41285f5f
invuzt <invuzt@gmail.com>
2026-03-31 13:26:00 +0700

    Final sync: all systems go for xpiz core

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,14 +1,18 @@
 LOCAL_PATH := $(call my-dir)
+
 include $(CLEAR_VARS)
 
+# Nama modul yang akan dipanggil di Java: System.loadLibrary("hello")
 LOCAL_MODULE    := hello
 LOCAL_SRC_FILES := hello.c
 LOCAL_LDLIBS    := -llog -landroid
-LOCAL_STATIC_LIBRARIES := cakru_core_static
+# Pakai library static xpiz yang baru
+LOCAL_STATIC_LIBRARIES := xpiz_core_static
 
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := cakru_core_static
-LOCAL_SRC_FILES := libcakru_core.a
+# Definisikan sumber file static dari Rust
+LOCAL_MODULE := xpiz_core_static
+LOCAL_SRC_FILES := libxpiz_core.a
 include $(PREBUILT_STATIC_LIBRARY)
