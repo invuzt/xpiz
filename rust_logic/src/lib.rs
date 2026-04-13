d340642b4c525fa66552fe2b087feea6c19a19f2
invuzt <invuzt@gmail.com>
2026-04-13 18:30:44 +0700

    feat: markdown saver core

--- rust_logic/src/lib.rs
+++ rust_logic/src/lib.rs
@@ -1,12 +1,29 @@
 use jni::JNIEnv;
-use jni::objects::{JClass};
+use jni::objects::{JClass, JString};
 use jni::sys::jstring;
+use std::fs;
 
 #[no_mangle]
 pub extern "C" fn Java_com_invuzt_xpiz_MainActivity_checkRustConnection(
     mut env: JNIEnv,
     _class: JClass,
 ) -> jstring {
-    let response = "Status: Rust Engine Connected! 🚀 (xpiz v1.0)";
-    env.new_string(response).expect("Gagal membuat string Java").into_raw()
+    let response = "Status: Rust Engine Connected! 🚀";
+    env.new_string(response).expect("Err").into_raw()
+}
+
+#[no_mangle]
+pub extern "C" fn Java_com_invuzt_xpiz_MainActivity_saveMarkdownNative(
+    mut env: JNIEnv,
+    _class: JClass,
+    path: JString,
+    content: JString,
+) -> jstring {
+    let file_path: String = env.get_string(&path).expect("Err").into();
+    let file_content: String = env.get_string(&content).expect("Err").into();
+
+    match fs::write(&file_path, &file_content) {
+        Ok(_) => env.new_string("✅ Tersimpan di internal storage").unwrap().into_raw(),
+        Err(e) => env.new_string(format!("❌ Gagal Simpan: {}", e)).unwrap().into_raw(),
+    }
 }
